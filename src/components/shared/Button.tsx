406656a0e4adf45d23c943ccccf6ea67971d1a8a
Emanuele Favero <emafaux89@gmail.com>
2025-08-11 01:48:43 +0200

    feat: add tailwind merge and cn util

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,4 +1,4 @@
-import clsx from 'clsx'
+import { cn } from '~/lib/utils'
 
 interface Props extends React.ComponentProps<'button'> {
   className?: string
@@ -8,7 +8,7 @@ interface Props extends React.ComponentProps<'button'> {
 export default function Button({ className, children, ...props }: Props) {
   return (
     <button
-      className={clsx(
+      className={cn(
         'mt-2 cursor-pointer rounded-full border border-blue-300 bg-blue-200 px-2.5 py-1 font-semibold text-blue-800 transition-transform duration-200 hover:bg-blue-800 hover:text-blue-200 active:scale-95',
         className,
       )}
